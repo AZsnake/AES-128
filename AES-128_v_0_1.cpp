eb0ee77f8303197d01fa9b30fe8c2dce345d9939
AZsnake <zsnake666@outlook.com>
2025-01-06 15:54:50 +0800

    Added the first draft for decryption function. Though it is not yet functional.

--- AES-128_v_0_1.cpp
+++ AES-128_v_0_1.cpp
@@ -30,6 +30,22 @@ uint8_t special_multiply_3(uint8_t to_be_multiplied)
 {
 	return special_multiply_2(to_be_multiplied) ^ to_be_multiplied;
 }
+uint8_t special_multiply_9(uint8_t to_be_multiplied)
+{
+	return special_multiply_3(special_multiply_3(to_be_multiplied));
+}
+uint8_t special_multiply_b(uint8_t to_be_multiplied) // 11
+{
+	return special_multiply_3(special_multiply_3(to_be_multiplied)) ^ special_multiply_2(to_be_multiplied);
+}
+uint8_t special_multiply_d(uint8_t to_be_multiplied) // 13
+{
+	return special_multiply_3(special_multiply_3(to_be_multiplied)) ^ special_multiply_2(special_multiply_2(to_be_multiplied));
+}
+uint8_t special_multiply_e(uint8_t to_be_multiplied) // 14
+{
+	return special_multiply_3(special_multiply_3(to_be_multiplied)) ^ special_multiply_3(to_be_multiplied) ^ special_multiply_2(to_be_multiplied); // Iteration of the functions above.
+}
 // Assemble 16 8-bit array into 4 32-bit array.
 uint32_t* convert_8_32(uint8_t* to_be_converted)
 {
@@ -117,6 +133,17 @@ uint8_t break_compare_sub(uint8_t to_be_broken)
 	seg[1] = (temp2 &= 0b00001111);
 	return sub_table[seg[0]][seg[1]];
 }
+// Break uint8_t into two 'uint4_t' and substitute them using the isub_table.
+uint8_t ibreak_compare_sub(uint8_t to_be_broken)
+{
+	uint8_t seg[2];
+	uint8_t temp1 = to_be_broken;
+	uint8_t temp2 = to_be_broken;
+	uint8_t max = 0, min = 0;
+	seg[0] = (temp1 >> 4);
+	seg[1] = (temp2 &= 0b00001111);
+	return isub_table[seg[0]][seg[1]];
+}
 // SubWord function, which substitutes the bytes for 32-bit array.
 uint32_t* SubWord(uint32_t* to_be_substituted)
 {
@@ -180,7 +207,7 @@ uint32_t RoundConstant(int number)
 			return 0x36000000;
 			break;
 		default:
-			perror("Rcon error.");
+			perror("RoundConstant error.");
 			return NULL;
 	}
 }
@@ -195,7 +222,12 @@ uint32_t* expand_calculation_key_44(uint8_t* original_key)
 		perror("Memory allocation failed.");
 		return NULL;
 	}
-	calculation_key_first = convert_8_32(original_key);
+	uint8_t* original_key_8 = (uint8_t*)malloc(sizeof(uint8_t) * 16);
+	for (int cyc = 0; cyc < 16; cyc++)
+	{
+		original_key_8[cyc] = original_key[cyc];
+	}
+	calculation_key_first = convert_8_32(original_key_8);
 	for (int cyc = 0; cyc < 4; cyc++)
 	{
 		W[cyc] = calculation_key_first[cyc];
@@ -244,68 +276,118 @@ uint8_t* AddRoundKey(uint8_t* to_be_added, uint32_t* key_W, int cyc)
 // Perform the SubByte operation.
 uint8_t* SubByte(uint8_t* to_be_substituted)
 {
-	uint8_t* Sb_0_15 = (uint8_t*)malloc(sizeof(uint8_t) * 16);
-	if (Sb_0_15 == NULL)
+	uint8_t* output = (uint8_t*)malloc(sizeof(uint8_t) * 16);
+	if (output == NULL)
 	{
 		perror("Memory allocation failed.");
 		return NULL;
 	}
 	for (int cyc = 0;cyc < 16;cyc++)
 	{
-		Sb_0_15[cyc] = break_compare_sub(to_be_substituted[cyc]);
+		output[cyc] = break_compare_sub(to_be_substituted[cyc]);
 	}
 	if (to_be_substituted)
 	{
 		free(to_be_substituted);
 	}
-	return Sb_0_15;
+	return output;
 }
-// Perform the rot calculation for Sc_0_15. Input should be Sb[0-3].
-uint8_t* ShiftRows(uint8_t* to_be_shifted)
+// Perform the inverse SubByte operation.
+uint8_t* iSubByte(uint8_t* to_be_isubstituted)
 {
-	uint8_t* Sc_8 = (uint8_t*)malloc(sizeof(uint8_t) * 16);
-	if (Sc_8 == NULL)
+	uint8_t* output = (uint8_t*)malloc(sizeof(uint8_t) * 16);
+	if (output == NULL)
 	{
 		perror("Memory allocation failed.");
 		return NULL;
 	}
-	Sc_8[0] = to_be_shifted[0];
-	Sc_8[1] = to_be_shifted[5];
-	Sc_8[2] = to_be_shifted[10];
-	Sc_8[3] = to_be_shifted[15];
-	Sc_8[4] = to_be_shifted[4];
-	Sc_8[5] = to_be_shifted[9];
-	Sc_8[6] = to_be_shifted[14];
-	Sc_8[7] = to_be_shifted[3];
-	Sc_8[8] = to_be_shifted[8];
-	Sc_8[9] = to_be_shifted[13];
-	Sc_8[10] = to_be_shifted[2];
-	Sc_8[11] = to_be_shifted[7];
-	Sc_8[12] = to_be_shifted[12];
-	Sc_8[13] = to_be_shifted[1];
-	Sc_8[14] = to_be_shifted[6];
-	Sc_8[15] = to_be_shifted[11]; // Shift row by brute force.
+	for (int cyc = 0; cyc < 16; cyc++)
+	{
+		output[cyc] = ibreak_compare_sub(to_be_isubstituted[cyc]);
+	}
+	if (to_be_isubstituted)
+	{
+		free(to_be_isubstituted);
+	}
+	return output;
+}
+// Perform the rotation calculation.
+uint8_t* ShiftRows(uint8_t* to_be_shifted)
+{
+	uint8_t* shifted = (uint8_t*)malloc(sizeof(uint8_t) * 16);
+	if (shifted == NULL)
+	{
+		perror("Memory allocation failed.");
+		return NULL;
+	}
+	shifted[0]  = to_be_shifted[0];
+	shifted[1]  = to_be_shifted[5];
+	shifted[3]  = to_be_shifted[15];
+	shifted[4]  = to_be_shifted[4];
+	shifted[5]  = to_be_shifted[9];
+	shifted[2]  = to_be_shifted[10];
+	shifted[6]  = to_be_shifted[14];
+	shifted[7]  = to_be_shifted[3];
+	shifted[8]  = to_be_shifted[8];
+	shifted[9]  = to_be_shifted[13];
+	shifted[10] = to_be_shifted[2];
+	shifted[11] = to_be_shifted[7];
+	shifted[12] = to_be_shifted[12];
+	shifted[13] = to_be_shifted[1];
+	shifted[14] = to_be_shifted[6];
+	shifted[15] = to_be_shifted[11]; // Shift row by brute force.
 	if (to_be_shifted)
 	{
 		free(to_be_shifted);
 	}
-	return Sc_8;
+	return shifted;
 }
-// Perform the mix calculation for Sd_0_15. Input should be Sc[0-15].
+// Perform the inverse shift row calculation.
+uint8_t* iShiftRows(uint8_t* to_be_ishifted)
+{
+	uint8_t* ishifted = (uint8_t*)malloc(sizeof(uint8_t) * 16);
+	if (ishifted == NULL)
+	{
+		perror("Memory allocation failed.");
+		return NULL;
+	}
+	ishifted[0]  = to_be_ishifted[0];
+	ishifted[1]  = to_be_ishifted[13];
+	ishifted[2]  = to_be_ishifted[10];
+	ishifted[3]  = to_be_ishifted[7];
+	ishifted[4]  = to_be_ishifted[4];
+	ishifted[5]  = to_be_ishifted[1];
+	ishifted[6]  = to_be_ishifted[14];
+	ishifted[7]  = to_be_ishifted[11];
+	ishifted[8]  = to_be_ishifted[8];
+	ishifted[9]  = to_be_ishifted[5];
+	ishifted[10] = to_be_ishifted[2];
+	ishifted[11] = to_be_ishifted[15];
+	ishifted[12] = to_be_ishifted[12];
+	ishifted[13] = to_be_ishifted[9];
+	ishifted[14] = to_be_ishifted[6];
+	ishifted[15] = to_be_ishifted[3]; // Reverse shift row by brute force.
+	if (to_be_ishifted)
+	{
+		free(to_be_ishifted);
+	}
+	return ishifted;
+}
+// Perform the mix column calculation.
 uint8_t* MixColumns(uint8_t* to_be_mixed)
 {
-	uint8_t* Sd_8 = (uint8_t*)malloc(sizeof(uint8_t) * 16);
-	if (Sd_8 == NULL)
+	uint8_t* output = (uint8_t*)malloc(sizeof(uint8_t) * 16);
+	if (output == NULL)
 	{
 		perror("Memory allocation failed.");
 		return NULL;
 	}
 	for (int cyc = 0; cyc < 4; cyc++)
 	{
-		Sd_8[0 + 4 * cyc] = special_multiply_2(to_be_mixed[0 + 4 * cyc]) ^ special_multiply_3(to_be_mixed[1 + 4 * cyc]) ^ to_be_mixed[2 + 4 * cyc] ^ to_be_mixed[3 + 4 * cyc];
-		Sd_8[1 + 4 * cyc] = to_be_mixed[0 + 4 * cyc] ^ special_multiply_2(to_be_mixed[1 + 4 * cyc]) ^ special_multiply_3(to_be_mixed[2 + 4 * cyc]) ^ to_be_mixed[3 + 4 * cyc];
-		Sd_8[2 + 4 * cyc] = to_be_mixed[0 + 4 * cyc] ^ to_be_mixed[1 + 4 * cyc] ^ special_multiply_2(to_be_mixed[2 + 4 * cyc]) ^ special_multiply_3(to_be_mixed[3 + 4 * cyc]);
-		Sd_8[3 + 4 * cyc] = special_multiply_3(to_be_mixed[0 + 4 * cyc]) ^ to_be_mixed[1 + 4 * cyc] ^ to_be_mixed[2 + 4 * cyc] ^ special_multiply_2(to_be_mixed[3 + 4 * cyc]);
+		output[0 + 4 * cyc] = special_multiply_2(to_be_mixed[0 + 4 * cyc]) ^ special_multiply_3(to_be_mixed[1 + 4 * cyc]) ^ to_be_mixed[2 + 4 * cyc] ^ to_be_mixed[3 + 4 * cyc];
+		output[1 + 4 * cyc] = to_be_mixed[0 + 4 * cyc] ^ special_multiply_2(to_be_mixed[1 + 4 * cyc]) ^ special_multiply_3(to_be_mixed[2 + 4 * cyc]) ^ to_be_mixed[3 + 4 * cyc];
+		output[2 + 4 * cyc] = to_be_mixed[0 + 4 * cyc] ^ to_be_mixed[1 + 4 * cyc] ^ special_multiply_2(to_be_mixed[2 + 4 * cyc]) ^ special_multiply_3(to_be_mixed[3 + 4 * cyc]);
+		output[3 + 4 * cyc] = special_multiply_3(to_be_mixed[0 + 4 * cyc]) ^ to_be_mixed[1 + 4 * cyc] ^ to_be_mixed[2 + 4 * cyc] ^ special_multiply_2(to_be_mixed[3 + 4 * cyc]);
 	}
 	/*
 	* The matrix above is: |02 03 01 01|
@@ -317,9 +399,37 @@ uint8_t* MixColumns(uint8_t* to_be_mixed)
 	{
 		free(to_be_mixed);
 	}
-	return Sd_8;
+	return output;
 }
-// Function that gets the final calculation result.
+// Perform the inverse mix column calculation.
+uint8_t* iMixColumns(uint8_t* to_be_imixed)
+{
+	uint8_t* output = (uint8_t*)malloc(sizeof(uint8_t) * 16);
+	if (output == NULL)
+	{
+		perror("Memory allocation failed.");
+		return NULL;
+	}
+	for (int cyc = 0; cyc < 4; cyc++)
+	{
+		output[0 + 4 * cyc] = special_multiply_e(to_be_imixed[0 + 4 * cyc]) ^ special_multiply_b(to_be_imixed[1 + 4 * cyc]) ^ special_multiply_d(to_be_imixed[2 + 4 * cyc]) ^ special_multiply_9(to_be_imixed[3 + 4 * cyc]);
+		output[1 + 4 * cyc] = special_multiply_9(to_be_imixed[0 + 4 * cyc]) ^ special_multiply_e(to_be_imixed[1 + 4 * cyc]) ^ special_multiply_b(to_be_imixed[2 + 4 * cyc]) ^ special_multiply_d(to_be_imixed[3 + 4 * cyc]);
+		output[2 + 4 * cyc] = special_multiply_d(to_be_imixed[0 + 4 * cyc]) ^ special_multiply_9(to_be_imixed[1 + 4 * cyc]) ^ special_multiply_e(to_be_imixed[2 + 4 * cyc]) ^ special_multiply_b(to_be_imixed[3 + 4 * cyc]);
+		output[3 + 4 * cyc] = special_multiply_b(to_be_imixed[0 + 4 * cyc]) ^ special_multiply_d(to_be_imixed[1 + 4 * cyc]) ^ special_multiply_9(to_be_imixed[2 + 4 * cyc]) ^ special_multiply_e(to_be_imixed[3 + 4 * cyc]);
+	}
+	/*
+	* The matrix above is: |0e 0b 0d 09|
+	*					   |09 0e 0b 0d|
+	*                      |0d 09 0e 0b|
+	*                      |0b 0d 09 0e|
+	*/
+	if (to_be_imixed)
+	{
+		free(to_be_imixed);
+	}
+	return output;
+}
+// Function that gets the final encryption result.
 uint32_t* encrypt_AES_128(uint8_t* original_key, uint32_t* to_be_encrypted)
 {
 	uint32_t* calculation_key;
@@ -347,6 +457,37 @@ uint32_t* encrypt_AES_128(uint8_t* original_key, uint32_t* to_be_encrypted)
 	state = SubByte(state);
 	state = ShiftRows(state);
 	state = AddRoundKey(state, calculation_key, 10);
+	uint32_t* state_32 = convert_8_32(state);
+	return state_32;
+}
+// Function that gets the final decryption result.
+uint32_t* decrypt_AES_128(uint8_t* original_key, uint32_t* to_be_decrypted)
+{
+	uint32_t* calculation_key;
+	calculation_key = expand_calculation_key_44(original_key);
+	uint8_t* state;
+	// Malloc calculation_key_32, which is a duplicate of the original calculation_key. So during the convert operation, calculation_key won't be freed.
+	uint32_t* calculation_key_32 = (uint32_t*)malloc(sizeof(uint32_t) * 44);
+	if (calculation_key_32 == NULL)
+	{
+		perror("Memory allocation failed.");
+		return NULL;
+	}
+	for (int cyc = 0; cyc < 44; cyc++)
+	{
+		*(calculation_key_32 + cyc) = *(calculation_key + cyc);
+	}
+	state = AddRoundKey(convert_32_8(calculation_key_32), to_be_decrypted, 10);
+	for (int cyc = 9; cyc > 0; cyc--)
+	{
+		state = iShiftRows(state);
+		state = iSubByte(state);
+		state = AddRoundKey(state, calculation_key, cyc);
+		state = iMixColumns(state);
+	}
+	state = iShiftRows(state);
+	state = iSubByte(state);
+	state = AddRoundKey(state, calculation_key, 0);
 	uint32_t* state_32 = convert_8_32(state);
 	return state_32;
 }
--- main.cpp
+++ main.cpp
@@ -28,11 +28,28 @@ int main(void)
     *(plain + 1) = 0x44556677;
     *(plain + 2) = 0x8899aabb;
     *(plain + 3) = 0xccddeeff;
+	uint32_t* decryption_test = (uint32_t*)malloc(sizeof(uint32_t) * 4);
+    if (decryption_test == NULL)
+    {
+		perror("Memory allocation failed.");
+		return 0;
+    }
+	*(decryption_test + 0) = 0x69c4e0d8;
+	*(decryption_test + 1) = 0x6a7b0430;
+	*(decryption_test + 2) = 0xd8cdb780;
+	*(decryption_test + 3) = 0x70b4c55a;
     uint32_t* encryption_result;
+	uint32_t* decryption_result;
 	encryption_result = encrypt_AES_128(original_key_test_8, plain);
+	decryption_result = decrypt_AES_128(original_key_test_8, decryption_test);
     for (int cyc = 0; cyc < 4; cyc++)
     {
         printf("%08x", encryption_result[cyc]);
+    }
+	printf("\n");
+    for (int cyc = 0; cyc < 4; cyc++)
+    {
+        printf("%08x", decryption_result[cyc]);
     }
     getchar();
     return 0;
